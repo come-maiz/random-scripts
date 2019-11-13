6a546bf1a014f66291ea36e19477a5cca4b8586b
Leo Arias <elopio@openzeppelin.com>
2019-11-13 17:54:21 +0000

    Update the case to delete anchors with blank target

--- openzeppelin/blog/html_cleaner.py
+++ openzeppelin/blog/html_cleaner.py
@@ -5,5 +5,6 @@ def clean(html_string):
     soup = bs4.BeautifulSoup(html_string, 'html.parser')
     anchors = soup.find_all('a')
     for anchor in anchors:
-        del anchor['id']
+        if anchor['target'] == '_blank':
+            del anchor['target']
     return str(soup)
--- openzeppelin/blog/tests/test_html_cleaner.py
+++ openzeppelin/blog/tests/test_html_cleaner.py
@@ -8,8 +8,11 @@ import html_cleaner
 class TestHTMLCleaner(testscenarios.WithScenarios, unittest.TestCase):
 
     scenarios = [
-        ('remove id from links', dict(
-            html_input='<a id="test">dummy</a>',
+        ('noop with target different than blank from links', dict(
+            html_input='<a target="test">dummy</a>',
+            expected_output='<a target="test">dummy</a>')),
+        ('remove targe blank from links', dict(
+            html_input='<a target="_blank">dummy</a>',
             expected_output='<a>dummy</a>')),
     ]
 
