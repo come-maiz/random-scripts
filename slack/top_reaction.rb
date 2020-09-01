460e57321e8edb795ea1f3caaffdf676c31a50d3
Santiago Palladino <spalladino@gmail.com>
2020-09-01 15:04:46 -0300

    Collect stats for all awards in a single run (#3)

--- slack/top_reaction.rb
+++ slack/top_reaction.rb
@@ -1,41 +1,55 @@
 require 'find'
 require 'json'
 
-if ARGV.length != 2
+if ARGV.length < 1
   puts 'Missing argument.'
-  puts 'Usage: ruby top_reaction.rb <path-to-slack-export> <reaction>'
+  puts 'Usage: ruby top_reaction.rb <path-to-slack-export> <reactions>'
   exit
 end
 
+AWARDS = %w(award_for_ability_to_ship award_for_healthy_workstyle award_for_intellectual_curiosity award_for_strong_sense_of_purpose award_for_commitment_to_excellence award_for_team_spirit thankyou-nico cv_whateverittakes)
+
 path = ARGV[0]
-reaction_name = ARGV[1]
+reaction_names = ARGV[1] ? ARGV[1].split(/\s+/) : AWARDS
 
-top_count = 0
-top_messages = []
+def msg_to_text(message, file)
+  "from #{message['user_profile']['name']} in #{file.split('/')[-2]} on #{File.basename(file, '.json')}\n\n#{message['text']}"
+end
 
-Find.find(path) do |file|
-  if File.extname(file) == '.json'
-    begin
-      parsed_json = JSON.parse(File.read(file))
-      parsed_json.each do |message|
-        if message['type'] == 'message'
-          message['reactions'].each do |reaction|
-            if reaction['name'] == reaction_name
-              reaction_count = reaction['count']
-              if reaction_count == top_count
-                top_messages.push(message['text'])
-              elsif reaction_count > top_count
-                top_count = reaction_count
-                top_messages = [message['text']]
+def stats(path, reaction_name)
+  top_count = 0
+  top_messages = []
+
+  Find.find(path) do |file|
+    if File.extname(file) == '.json'
+      begin
+        parsed_json = JSON.parse(File.read(file))
+        parsed_json.each do |message|
+          if message['type'] == 'message'
+            message['reactions'].each do |reaction|
+              if reaction['name'] == reaction_name
+                reaction_count = reaction['count']
+                if reaction_count == top_count
+                  top_messages.push(msg_to_text(message, file))
+                elsif reaction_count > top_count
+                  top_count = reaction_count
+                  top_messages = [msg_to_text(message, file)]
+                end
               end
             end
           end
         end
+      rescue => e
       end
-    rescue => e
     end
   end
+
+  [top_count, top_messages]
 end
 
-puts top_count
-puts top_messages
+reaction_names.each do |reaction_name|
+  top_count, top_messages = stats(path, reaction_name)
+  puts "#{reaction_name}: #{top_count}"
+  puts top_messages.join("\n\n")
+  puts "\n#{"*" * 40}\n"
+end
